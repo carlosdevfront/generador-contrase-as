f5820745572adcaa83ea248349b45943697dd18b
Carlos Arias <carlox952010@hotmail.com>
2025-01-30 13:07:35 -0500

    Generador de contraseñas

---
+++ Javascript/funcion.js
@@ -0,0 +1,9 @@
+
+function generatePassword() {
+    const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*";
+    let password = "";
+    for (let i = 0; i < 12; i++) {
+        password += characters.charAt(Math.floor(Math.random() * characters.length));
+    }
+    document.getElementById("passwordBox").value = password;
+}
